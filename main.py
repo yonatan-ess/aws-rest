c8c66fb34325246ce67993017df4b44767062365
yoni <yonatan@penguin-it.co.il>
2016-05-23 01:00:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys, getopt
+from instance import *
+
+if len(sys.argv) <= 3:
+	print "credoaws.py [num of instance to launch] [dns record type] [dns name]"
+	sys.exit(0)
+
+
+if __name__ == "__main__": 
+
+	number_of_instance=sys.argv[1]
+	dns_type=sys.argv[2]
+	dns_name=sys.argv[3]
+
+
+	a = Instance()
+	a.name=dns_name
+	a.dns_type=dns_type
+	
+	a.runInstance()
